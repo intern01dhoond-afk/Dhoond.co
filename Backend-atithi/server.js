ed6f40cc5f8536d7cd0b22e1de3e17240779fa2f
itshemanthcode <balarajk32@gmail.com>
2026-04-17 02:24:29 +0530

    fix: explicit 0.0.0.0 bind for container networking

--- Backend-atithi/server.js
+++ Backend-atithi/server.js
@@ -12,7 +12,7 @@ pool
   .then(() => {
     console.log("DB Connected ");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT} `);
     });
   })
